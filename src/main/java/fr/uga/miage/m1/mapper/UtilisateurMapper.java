e446994b712c9d95ba25c0819b071f875cb2fa7e
l3miage-chazeauv <vincent.chazeau05@gmail.com>
2024-01-23 11:35:10 +0100

    Ajout fichiers mapper

--- src/main/java/fr/uga/miage/m1/mapper/UtilisateurMapper.java
+++ src/main/java/fr/uga/miage/m1/mapper/UtilisateurMapper.java
@@ -2,17 +2,20 @@ package fr.uga.miage.m1.mapper;
 
 import fr.uga.miage.m1.dto.UtilisateurDTO;
 import fr.uga.miage.m1.model.Utilisateur;
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+import java.util.List;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface UtilisateurMapper {
 
-    UtilisateurMapper INSTANCE = Mappers.getMapper(UtilisateurMapper.class);
+    Utilisateur dtoToEntity(UtilisateurDTO utilisateurDTO);
 
-    // @Mapping(target = "motDePasse", ignore = true) // Ignorer le mot de passe lors de la conversion
-    // UtilisateurDTO utilisateurToDTO(Utilisateur utilisateur);
+    UtilisateurDTO entityToDTO(Utilisateur utilisateur);
 
-    Utilisateur dtoToUtilisateur(UtilisateurDTO utilisateurDTO);
+    List<Utilisateur> dtoToEntity(List<UtilisateurDTO> utilisateurDTOs);
+
+    List<UtilisateurDTO> entityToDTO(List<Utilisateur> utilisateurs);
 }
 
