33ebebc56317abef4bc73ed9f0d1813c95a5dbdf
l3miage-chazeauv <vincent.chazeau05@gmail.com>
2024-01-23 11:11:43 +0100

    Ajustement EtapeDTO

--- src/main/java/fr/uga/miage/m1/dto/EtapeDTO.java
+++ src/main/java/fr/uga/miage/m1/dto/EtapeDTO.java
@@ -7,11 +7,18 @@ import fr.uga.miage.m1.model.PanierEtape;
 import java.util.List;
 
 public class EtapeDTO {
+
     private long idLieu;
+
     private long idCovoiturage;
+    
     private float prixEtape;
+    
     private int dureeDepuisDepart;
+    
     private LieuDTO lieu;
+    
     private CovoiturageDTO covoiturage;
+    
     private List<PanierEtapeDTO> panierEtapes;
 }
