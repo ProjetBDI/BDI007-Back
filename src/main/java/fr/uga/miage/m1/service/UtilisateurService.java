342b7d875e5f0af1dcd9d6af8c7228f25a72a07c
l3miage-delagral <alex.del57640@gmail.com>
2024-01-23 09:24:21 +0100

    update Festivals.csv en bd

--- src/main/java/fr/uga/miage/m1/service/UtilisateurService.java
+++ src/main/java/fr/uga/miage/m1/service/UtilisateurService.java
@@ -1,18 +1,25 @@
 package fr.uga.miage.m1.service;
 
+import fr.uga.miage.m1.model.Utilisateur;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.uga.miage.m1.repository.UtilisateurRepository;
 
+import java.util.Optional;
+
 @Service
 public class UtilisateurService {
 
+    @Autowired
     private final UtilisateurRepository utilisateurRepository;
 
-    @Autowired
     public UtilisateurService(UtilisateurRepository utilisateurRepository) {
         this.utilisateurRepository = utilisateurRepository;
+    }
+
+    public Optional<Utilisateur> getUserByUsername(String username) {
+        return utilisateurRepository.findByUsername(username);
     }
 
     // Ajouter des méthodes spécifiques de service ici si nécessaire
