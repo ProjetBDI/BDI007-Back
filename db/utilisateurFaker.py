f1a6c857227834812401021d6db432cbb560883f
arthurAlade <arthuraladenise@gmail.com>
2024-01-24 11:00:28 +0100

    chores(peuplement): add id and phone number to user

--- db/utilisateurFaker.py
+++ db/utilisateurFaker.py
@@ -2,19 +2,20 @@ import csv
 from faker import Faker
 import random
 
-fake = Faker()
-
+fake = Faker('fr_FR')
 # Nombre d'utilisateurs à générer
 nombre_utilisateurs = 100000
 
 # Génération des données
 utilisateurs = []
 for _ in range(nombre_utilisateurs):
+    identifiant = _
     nom = fake.last_name()
     prenom = fake.first_name()
     email = f"{prenom.lower()}.{nom.lower()}{_}@{fake.free_email_domain()}"
     date_naissance = fake.date_of_birth().strftime('%Y-%m-%d')
-    utilisateurs.append([email, nom, prenom, date_naissance])
+    telephone = fake.phone_number()
+    utilisateurs.append([identifiant,email, nom, prenom, date_naissance, telephone])
 
 # Écriture des données dans un fichier CSV
 nom_fichier_csv = 'utilisateurs.csv'
