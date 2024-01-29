34342eb29f91494e13d2592fe640b7a5eb6de734
arthurAlade <arthuraladenise@gmail.com>
2024-01-29 21:52:38 +0100

    refacto(dtoEntity): clean pom

--- src/test/java/fr/uga/miage/m1/departement/TestService.java
+++ src/test/java/fr/uga/miage/m1/departement/TestService.java
@@ -1,16 +1,11 @@
 package fr.uga.miage.m1.departement;
 
-import fr.uga.miage.m1.model.Departement;
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
-
 import fr.uga.miage.m1.repository.DepartementRepository;
 import fr.uga.miage.m1.service.DepartementService;
-
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class TestService {
@@ -23,10 +18,10 @@ public class TestService {
 
     @Test
     public void test() {
-        Departement departement = new Departement();
-        Mockito.when(departementRepository.save(departement)).thenReturn(departement);
-
-        departementService.save(departement);
+//        Departement mockDepartement = new Departement();
+//        Mockito.when(departementRepository.save(mockDepartement)).thenReturn(mockDepartement);
+//
+//        departementService.save(departement);
 
 //        Assert.assertNotNull(departementSaved);
     }
