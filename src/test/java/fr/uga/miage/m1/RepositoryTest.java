7be6bbd3781288908e335c65e92275a1ae8b9c91
l3miage-charrek <charre.kyllian@gmail.com>
2024-02-01 21:25:51 +0100

    Test IdCommune et IdDepartement

--- src/test/java/fr/uga/miage/m1/RepositoryTest.java
+++ src/test/java/fr/uga/miage/m1/RepositoryTest.java
@@ -5,6 +5,7 @@ import fr.uga.miage.m1.model.*;
 
 import fr.uga.miage.m1.repository.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,29 +21,30 @@ import java.util.*;
 class RepositoryTest {
 
     @Autowired
-    private CommuneRepository communeRepository;
+    CommuneRepository communeRepository;
 
 //    @Autowired
 //    DepartementService departementService;
 
     @Autowired
-    private DepartementRepository departementRepository;
+    DepartementRepository departementRepository;
 
     @Autowired
-    private UtilisateurRepository utilisateurRepository;
+    private  static UtilisateurRepository utilisateurRepository;
 
 
-    private final List<Commune> communes = new ArrayList<>();
+    private static List<Commune> communes = new ArrayList<>();
 
-    private final List<Festival> festivals = new ArrayList<>();
-    private final Commune commune = new Commune();
-    private final Departement departement = new Departement();
-    private final Festival festival = new Festival();
-    private final Domaine domaine = new Domaine();
+    private static List<Festival> festivals = new ArrayList<>();
+    private static Commune commune = new Commune();
+    private Departement departement = new Departement();
+    private static Festival festival = new Festival();
+    private static Domaine domaine = new Domaine();
 
-    private final Utilisateur utilisateur = new Utilisateur();
+    private static Utilisateur utilisateur = new Utilisateur();
 
-    RepositoryTest() {
+    @BeforeAll
+    void initialisation() {
 
         Calendar calendar = Calendar.getInstance();
         calendar.set(2022, Calendar.JANUARY, 1);
@@ -50,67 +52,71 @@ class RepositoryTest {
 
         calendar.set(2023, Calendar.AUGUST, 15);
         Date dateFin = calendar.getTime();
+        utilisateur.setEmail("test@test.com");
+        utilisateur.setNom("TestNom");
+        utilisateur.setPrenom("TestPrenom");
+        utilisateur.setMotDePasse("TestMotDePasse");
+        utilisateur.setDateNaissance(dateDebut);
+        utilisateur.setTelephone("0123456789");
 
-        this.utilisateur.setEmail("test@test.com");
-        this.utilisateur.setNom("TestNom");
-        this.utilisateur.setPrenom("TestPrenom");
-        this.utilisateur.setMotDePasse("TestMotDePasse");
-        this.utilisateur.setDateNaissance(dateDebut);
-        this.utilisateur.setTelephone("0123456789");
+        departement.setIdDepartement(1);
+        departement.setNomDepartement("TestDepartement");
+        departement.setNumDepartement("01");
+        departement.setNomRegion("TestRegion");
+        departementRepository.save(departement);
 
-
-        this.commune.setNomCommune("TestVille");
-        this.commune.setCodePostal("12345");
-        this.commune.setCodeINSEE("123456789");
-        this.commune.setLongitude("1.0");
-        this.commune.setLatitude("1.0");
-
-        this.communes.add(this.commune);
-
-        this.departement.setNomDepartement("TestDepartement");
-        this.departement.setCommunes(communes);
-        this.departement.setNumDepartement("01");
-        this.departement.setNomRegion("TestRegion");
-
-        this.domaine.setNomDomaine("TestDomaine");
-        this.domaine.setSousDomaines("TestSousDomaine");
-
-        this.festival.setNom("TestFestival");
-
-
-        this.festival.setDateDebut(dateDebut);
-        this.festival.setDateFin(dateFin);
-        this.festival.setSiteWeb("www.test.com");
-        this.festival.setLieuPrincipal("TestLieu");
-        this.festival.setNbPassTotal(100);
-        this.festival.setNbPassIndispo(25);
-        this.festival.setNbPassDispo(50);
-        this.festival.setTarifPass(25);
-        this.festival.setStatus(FestivalStatus.OUVERT);
-        this.festival.setIdCommune(commune);
-
-        this.festival.setIdDomaine(this.domaine);
-
-        this.festivals.add(this.festival);
-        this.domaine.setFestivals(this.festivals);
-
-
-
-
-    }
-
-    @Test
-    void findByIdCommuneTest() {
-        Commune commune = new Commune();
         commune.setCodeINSEE("123456789");
         commune.setCodePostal("12345");
         commune.setNomCommune("TestVille");
         commune.setLongitude("1.0");
         commune.setLatitude("1.0");
         commune.setIdDepartement(departement);
-
-        departementRepository.save(departement);
         communeRepository.save(commune);
+
+        communes.add(commune);
+
+        departement.setNomDepartement("TestDepartement");
+        departement.setCommunes(communes);
+        departement.setNumDepartement("01");
+        departement.setNomRegion("TestRegion");
+
+        domaine.setNomDomaine("TestDomaine");
+        domaine.setSousDomaines("TestSousDomaine");
+
+        festival.setNom("TestFestival");
+
+
+        festival.setDateDebut(dateDebut);
+        festival.setDateFin(dateFin);
+        festival.setSiteWeb("www.test.com");
+        festival.setLieuPrincipal("TestLieu");
+        festival.setNbPassTotal(100);
+        festival.setNbPassIndispo(25);
+        festival.setNbPassDispo(50);
+        festival.setTarifPass(25);
+        festival.setStatus(FestivalStatus.OUVERT);
+        festival.setIdCommune(commune);
+
+        festival.setIdDomaine(domaine);
+
+        festivals.add(festival);
+        domaine.setFestivals(festivals);
+
+    }
+
+    @Test
+    void findByIdDepartementTest() {
+
+        // When
+        Departement foundDepartement = departementRepository.findById((long)departement.getIdDepartement()).orElse(null);
+
+        // Then
+        Assertions.assertNotNull(foundDepartement);
+        Assertions.assertEquals(departement.getIdDepartement(), foundDepartement.getIdDepartement());
+    }
+
+    @Test
+    void findByIdCommuneTest() {
 
         // When
         Commune foundCommune = communeRepository.findById(commune.getIdCommune()).orElse(null);
@@ -120,17 +126,49 @@ class RepositoryTest {
         Assertions.assertEquals(commune.getIdCommune(), foundCommune.getIdCommune());
     }
 
+
+
     @Test
     void findByIdUtilisateurTest() {
-
-        utilisateurRepository.save(this.utilisateur);
 
         Utilisateur foundUtilisateur = utilisateurRepository.findById(this.utilisateur.getIdUtilisateur()).orElse(null);
 
         //then
         Assertions.assertNotNull(foundUtilisateur);
-        Assertions.assertEquals(this.utilisateur.getIdUtilisateur(), foundUtilisateur.getIdUtilisateur());
+        Assertions.assertEquals(utilisateur.getIdUtilisateur(), foundUtilisateur.getIdUtilisateur());
 
     }
+
+//    @Test
+//    void findByIdCovoiturageTest() {
+//
+//        Covoiturage foundCovoiturage = covoiturageRepository.findById(this.covoiturage.getIdCovoiturage()).orElse(null);
+//
+//        //then
+//        Assertions.assertNotNull(foundCovoiturage);
+//        Assertions.assertEquals(this.covoiturage.getIdCovoiturage(), foundCovoiturage.getIdCovoiturage());
+//
+//    }
+
+//    @Test
+//    void findByIdDomaineTest() {
+//
+//        Domaine foundDomaine = domaineRepository.findById((long) this.domaine.getIdDomaine()).orElse(null);
+//
+//        //then
+//        Assertions.assertNotNull(foundDomaine);
+//        Assertions.assertEquals(this.domaine.getIdDomaine(), foundDomaine.getIdDomaine());
+//    }
+
+//    @Test
+//    void findByIdFestivalTest() {
+//
+//        Festival foundFestival = festivalRepository.findById(this.festival.getIdFestival()).orElse(null);
+//
+//        //then
+//        Assertions.assertNotNull(foundFestival);
+//        Assertions.assertEquals(this.festival.getIdFestival(), foundFestival.getIdFestival());
+//
+//    }
 
 }
