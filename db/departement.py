680e657f8d848ee1b886f3ebc3b7c6b493d81cf1
arthurAlade <arthuraladenise@gmail.com>
2024-02-02 10:43:12 +0100

    remove(mainWithoutCSVandPY): remove csv and python codes

--- db/departement.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df_departement = pd.read_csv('Departement.csv', encoding='ISO-8859-1')
-
-df_departement['id_department'] = [i for i in range(len(df_departement))]
-
-
-    
-df_departement.to_csv('Departement1.csv', index=False, encoding='ISO-8859-1')
--- db/liaisonCommuneDepartement.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df_commune = pd.read_csv('Commune.csv', encoding='ISO-8859-1')
-df_departement = pd.read_csv('Departement1.csv', encoding='ISO-8859-1')
-
-
-df_merge = pd.merge(df_commune, df_departement, on='departement', how='inner')
-df_merge.to_csv('LiaisonCommuneDepartement.csv', index=False, encoding='ISO-8859-1')
